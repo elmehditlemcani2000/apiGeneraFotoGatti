1640a40455339b971aa7bc89a96f4653b41686db
elmehditlemcani2000 <elmehditlemcani@gmail.com>
2022-06-23 12:31:13 +0200

    Initial Commit

---
+++ apiGeneraFotoGatti/apiGeneraFotoGattiApp.swift
@@ -0,0 +1,17 @@
+//
+//  apiGeneraFotoGattiApp.swift
+//  apiGeneraFotoGatti
+//
+//  Created by user on 23/06/22.
+//
+
+import SwiftUI
+
+@main
+struct apiGeneraFotoGattiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
